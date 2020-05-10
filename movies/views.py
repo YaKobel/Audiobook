1af033dfd3a01ce341a6d6e657797197138d6c5d
Yaroslav Kobel <yarokobel@gmail.com>
2020-05-10 23:12:08 +0300

    add Search

--- movies/views.py
+++ movies/views.py
@@ -117,3 +117,14 @@ class AddStarRating(View):
         else:
             return HttpResponse(status=400)
 
+class Search(ListView):
+    """Поиск фильмов"""
+    paginate_by = 3
+
+    def get_queryset(self):
+        return Movie.objects.filter(title__icontains=self.request.GET.get("q"))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["q"] = f'q={self.request.GET.get("q")}&'
+        return context
